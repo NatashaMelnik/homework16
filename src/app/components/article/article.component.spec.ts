7bbb774b2f8608935486d70f05348181487000b0
NatashaMelnik <n.melnik1788@gmail.com>
2021-05-23 23:24:04 +0300

    article test

--- src/app/components/article/article.component.spec.ts
+++ src/app/components/article/article.component.spec.ts
@@ -1,10 +1,16 @@
-import { ComponentFixture, TestBed } from '@angular/core/testing';
+// import { Observable } from 'rxjs/Observable';
+import 'rxjs';
+// import { NO_ERRORS_SCHEMA } from '@angular/core';
+import { ComponentFixture, TestBed, async } from '@angular/core/testing';
+import { PageTextService } from '../../services/page-text.service';
+import 'rxjs/add/observable/of';
 
 import { ArticleComponent } from './article.component';
 
 describe('ArticleComponent', () => {
   let component: ArticleComponent;
   let fixture: ComponentFixture<ArticleComponent>;
+  let spy: jasmine.Spy;
 
   beforeEach(async () => {
     await TestBed.configureTestingModule({
@@ -16,10 +22,23 @@ describe('ArticleComponent', () => {
   beforeEach(() => {
     fixture = TestBed.createComponent(ArticleComponent);
     component = fixture.componentInstance;
+    // PageTextService = fixture.debugElement.injector.get(PageTextService);
+    // spy = spyOn(PageTextService, 'getListTexts');
     fixture.detectChanges();
   });
 
   it('should create', () => {
     expect(component).toBeTruthy();
   });
+
+  // it('should call ViewChild element sliderMiniat', () => {
+  //   expect(component.sliderMiniat).toBe('div');
+  // });
+
+  it('should call service with texts', () => {
+    component.constructor();
+    expect(spy.calls.any()).toBeTruthy();
+  });
+
+
 });

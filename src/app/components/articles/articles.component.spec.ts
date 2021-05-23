c48724cabc76da04936b230c288f23d6f9d81c3a
NatashaMelnik <n.melnik1788@gmail.com>
2021-05-23 23:45:43 +0300

    articles rout test

--- src/app/components/articles/articles.component.spec.ts
+++ src/app/components/articles/articles.component.spec.ts
@@ -1,14 +1,18 @@
 import { ComponentFixture, TestBed } from '@angular/core/testing';
+import { RouterTestingModule } from '@angular/router/testing';
+import { HttpClientModule } from '@angular/common/http';
 
 import { ArticlesComponent } from './articles.component';
 
 describe('ArticlesComponent', () => {
   let component: ArticlesComponent;
   let fixture: ComponentFixture<ArticlesComponent>;
+  const testUrl = 'article#';
 
   beforeEach(async () => {
     await TestBed.configureTestingModule({
-      declarations: [ ArticlesComponent ]
+      declarations: [ ArticlesComponent ],
+      imports: [HttpClientModule, RouterTestingModule.withRoutes([])]
     })
     .compileComponents();
   });
@@ -22,4 +26,11 @@ describe('ArticlesComponent', () => {
   it('should create', () => {
     expect(component).toBeTruthy();
   });
+
+  it('should return route path', () => {
+    const arrayPath = component.goToArticle(testUrl);
+    expect(arrayPath).not.toBeNull();
+    expect(arrayPath).not.toBeUndefined();
+  });
+
 });
